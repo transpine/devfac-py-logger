ae11fb0b2b78e5a5be9183c022c900ee9a17b95a
transpine <transpine@gmail.com>
2021-11-09 23:04:36 +0900

    - initial commit - version 0.2 - remove filename, linenumber when it's ipython

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# t-logger
+# Simple_py_logger
 Simple, Colored, File and Line number printable python logger
+
+I wanted to log like this
+1. colored 
+2. file name
+3. line number
+4. time
+5. print type of logs by different func name
+
+```
+[2021-11-09 22:53:41.227][INF][test_logger.py:39] info log
+[2021-11-09 22:53:41.229][DBG][test_logger.py:40] debug log
+[2021-11-09 22:53:41.230][ERR][test_logger.py:41] error log
+[2021-11-09 22:53:41.231][WRN][test_logger.py:42] warning log
+[2021-11-09 22:53:41.233][CRC][test_logger.py:43] critical log
+```
+
+## How to use
+```
+from simple_py_logger import log
+
+log.info('info log')
+log.debug('debug log')
+log.error('error log')
+log.warning('warning log')
+log.critical('critical log')
+```
+
+## features
+- 0.2
+  - At jupyter notebook there will be NO filename and line number
+    ```
+    [2021-11-09 23:01:34.591][WRN] warning log
+    ```
---
+++ __init__.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# File: __init__.py
+# Project: simple_py_logger
+# Created Date: 2021-11-09 Tuesday 10:46:38
+# Author: transpine(transpine@gmail.com)
+# ----------------------------------------------
+# Last Modified: 2021-11-09 Tuesday 11:01:24
+# Modified By: transpine
+# ----------------------------------------------
+# Copyright (c) 2021 devfac
+# 
+# MIT License
+# 
+# Copyright (c) 2021 devfac
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###
+import logging
+import inspect
+import os
+
+from IPython import get_ipython
+from colorlog import ColoredFormatter
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = ColoredFormatter(
+    "%(log_color)s[%(asctime)s.%(msecs)03d][%(levelname)s]%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    reset=True,
+    log_colors={
+      'DBG': 'cyan',
+      'INF': 'white,bold',
+      'WRN': 'yellow',
+      'ERR': 'red,bold',
+      'CRC': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
+)
+ch.setFormatter(formatter)
+
+log = logging.getLogger('simple_py_logger')
+log.setLevel(logging.DEBUG)
+log.handlers = []       # No duplicated handlers
+log.propagate = False   # workaround for duplicated logs in ipython
+log.addHandler(ch)
+
+logging.addLevelName(logging.DEBUG + 1, 'DBG')
+logging.addLevelName(logging.INFO + 1, 'INF')
+logging.addLevelName(logging.WARNING + 1, 'WRN')
+logging.addLevelName(logging.ERROR + 1, 'ERR')
+logging.addLevelName(logging.CRITICAL + 1, 'CRC')
+
+def ipython_check():
+  shell = get_ipython().__class__.__name__
+  if shell == 'ZMQInteractiveShell' or shell == 'TerminalInteractiveShell':
+    return True
+  else:
+    return False
+
+def dbg(self, msg, *args, **kwargs):   
+  stack = inspect.stack()
+  filepath = stack[1][1]
+  lineno = stack[1][2]
+  if ipython_check():
+    self.log(logging.DEBUG + 1, f' {msg}', *args, **kwargs)
+  else:
+    self.log(logging.DEBUG + 1, f'[{os.path.basename(filepath)}:{lineno}] {msg}', *args, **kwargs)
+def inf(self, msg, *args, **kwargs):
+  stack = inspect.stack()
+  filepath = stack[1][1]
+  lineno = stack[1][2]
+  if ipython_check():
+    self.log(logging.INFO + 1, f' {msg}', *args, **kwargs)
+  else:    
+    self.log(logging.INFO + 1, f'[{os.path.basename(filepath)}:{lineno}] {msg}', *args, **kwargs)
+def wrn(self, msg, *args, **kwargs):
+  stack = inspect.stack()
+  filepath = stack[1][1]
+  lineno = stack[1][2]
+  if ipython_check():
+    self.log(logging.WARNING + 1, f' {msg}', *args, **kwargs)
+  else:
+    self.log(logging.WARNING + 1, f'[{os.path.basename(filepath)}:{lineno}] {msg}', *args, **kwargs)
+def err(self, msg, *args, **kwargs):
+  stack = inspect.stack()
+  filepath = stack[1][1]
+  lineno = stack[1][2]
+  if ipython_check():
+    self.log(logging.ERROR + 1, f' {msg}', *args, **kwargs)
+  else:
+    self.log(logging.ERROR + 1, f'[{os.path.basename(filepath)}:{lineno}] {msg}', *args, **kwargs)
+def crc(self, msg, *args, **kwargs):
+  stack = inspect.stack()
+  filepath = stack[1][1]
+  lineno = stack[1][2]
+  if ipython_check():
+    self.log(logging.CRITICAL + 1, f' {msg}', *args, **kwargs)
+  else:
+    self.log(logging.CRITICAL + 1, f'[{os.path.basename(filepath)}:{lineno}] {msg}', *args, **kwargs)
+
+logging.Logger.debug = dbg
+logging.Logger.info = inf
+logging.Logger.warning = wrn
+logging.Logger.error = err
+logging.Logger.critical = crc
+
+__all__ = ['log']
